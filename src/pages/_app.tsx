8af86c04f4db91161cd2f2cf82a5bf080636f061
polunwu <polunwu@gmail.com>
2024-06-23 16:47:43 +0800

    chore(): add html font as rokkitt

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Rokkitt } from 'next/font/google';
+
+const rokkitt = Rokkitt({ subsets: ['latin'], display: 'swap' });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${rokkitt.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
