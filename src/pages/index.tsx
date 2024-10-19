6621b47aed87b6aef159c7b34e6d4e69cedca7cb
polunwu <polunwu@gmail.com>
2024-10-19 21:51:11 +0800

    feat(Home): add specific absolute layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,18 +14,28 @@ export default function Home() {
           />
         </div>
         <div className='flex-auto relative max-h-[16%] flex'>
-          <div className='flex-auto max-w-[14%] bg-primary'>我是房東</div>
+          <div className='flex-auto max-w-[14%] bg-primary relative'>
+            <div className='absolute -top-[43%] right-0 bottom-0 left-0 bg-primary'>
+              我是房東
+            </div>
+          </div>
           <div className='flex-auto bg-secondary'>Taipei Dreamhouse</div>
           <div className='flex-auto max-w-[31%] bg-secondary'>重設代碼</div>
         </div>
       </section>
       {/* Right */}
       <section className='flex-auto max-w-[30%] flex flex-col'>
-        <div className='flex-auto bg-primary max-h-[26%]'>租客代碼</div>
-        <div className='flex-auto bg-primary max-h-[14%]'>
-          <span>(a</span>
-          <span>000000000</span>
-          <span>)</span>
+        <div className='flex-auto bg-primary max-h-[26%] relative'>
+          <div className='absolute top-0 right-0 bottom-0 -left-[10%] bg-primary'>
+            租客代碼
+          </div>
+        </div>
+        <div className='flex-auto bg-primary max-h-[14%] relative'>
+          <div className='absolute top-0 right-0 bottom-0 -left-1/3 bg-primary'>
+            <span>(a</span>
+            <span>000000000</span>
+            <span>)</span>
+          </div>
         </div>
         <div className='flex-auto bg-secondary max-h-[25%]'>登入找房</div>
         <div className='flex-auto relative'>
